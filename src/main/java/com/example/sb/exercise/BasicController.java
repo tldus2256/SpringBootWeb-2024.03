7ffd2e399bb554ea6924dfddc8e8b434b9972da1
tldus2256 <155405754+tldus2256@users.noreply.github.com>
2024-03-06 23:56:26 +0900

    Day 2

--- src/main/java/com/example/sb/exercise/BasicController.java
+++ src/main/java/com/example/sb/exercise/BasicController.java
@@ -1,15 +1,91 @@
 package com.example.sb.exercise;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.mindrot.jbcrypt.BCrypt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 @RequestMapping("/ex")
 public class BasicController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @GetMapping("/hello")		//localhost:8090/sb/ex/hello
     public String hello() {
         return "exercise/hello";	// hello.html
     }
+
+    @ResponseBody                   //html파일을 찾지 말고, 데이터를 직접 전송
+    @GetMapping("/noHtml")
+    public String noHtml() {
+        return "<h1>Hello Spring Boot!!!</h1>";
+    }
+
+    @GetMapping("/redirect")
+    public String redirect() {
+        return "redirect:/ex/hello";        // Redirection
+    }
+
+    @GetMapping("/params")
+    public String params(Model model){
+        model.addAttribute("name","제임스");
+        return "exercise/params";
+    }
+
+    @GetMapping("/params2")
+    public String params2(Model model, HttpServletRequest req){                //java.net.http.HttpRequest
+        String name = req.getParameter("name");
+        model.addAttribute("name",name);
+        return "exercise/params";
+    }
+
+    @GetMapping("/params3")
+    public String params3(Model model, String name, int count){
+        model.addAttribute("name",name+count);
+        return "exercise/params";
+    }
+
+    @GetMapping("/memberForm")
+    public String memberForm() {
+        return "exercise/memberForm";
+    }
+
+    @PostMapping("/memberProc")
+    public String memberProc(Member member,Model model) {
+        log.info(member.toString());
+        model.addAttribute("name",member.getName());
+        return "exercise/params";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "exercise/login";
+    }
+
+    @PostMapping("/login")
+    public String loginProc(String uid, String pwd, HttpSession session, Model model) {
+        String hashedPwd = "$2a$10$6yEltiYs7hI.hXXmVkA0YeIytAjYKkaU.tqt7WJMqWndwUg4X8sji";
+        if (uid.equals("james")&& BCrypt.checkpw(pwd,hashedPwd)) {
+            model.addAttribute("msg",uid + "님이 로그인했습니다.");
+            session.setAttribute("sessUid",uid);
+            session.setAttribute("sessUname","제임스");
+            return "exercise/loginResult";
+        } else {
+            model.addAttribute("msg","uid, 비밀번호를 확인하세요.");
+            return "exercise/loginResult";
+        }
+    }
+
+    @ResponseBody
+    @GetMapping(value = {"/path/{uid}/{bid}","/path/{uid}"})
+    public String path(@PathVariable String uid, @PathVariable (required = false)Integer bid) {
+        bid = (bid == null) ? 0 : bid;
+        return "<h1>uid=" + uid + ", bid=" + bid + "</h1>";
+    }
+
 }
---
+++ src/main/java/com/example/sb/users/UserServiceImpl.java
@@ -0,0 +1,54 @@
+package com.example.sb.users;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService{
+    @Autowired
+    private UserDao uDao;
+    @Override
+    public User getUserByUid(String uid) {
+        User user = uDao.getUser(uid);
+        return user;
+    }
+
+    @Override
+    public List<User> getUserList(int page) {
+        int offset = (page - 1) * COUNT_PER_PAGE;
+        return uDao.getUserList(COUNT_PER_PAGE,offset);
+    }
+
+    @Override
+    public int getUserCount() {
+        return uDao.getUserCount();
+    }
+
+    @Override
+    public void registerUser(User user) {
+        uDao.insertUser(user);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        uDao.updateUser(user);
+    }
+
+    @Override
+    public void deleteUser(String uid) {
+        uDao.deleteUser(uid);
+    }
+
+    @Override
+    public int login(String uid, String pwd) {
+        User user = uDao.getUser(uid);
+        if (user == null)
+            return USER_NOT_EXIST;
+        if (BCrypt.checkpw(pwd, user.getPwd()))
+            return CORRECT_LOGIN;
+        return WRONG_PASSWORD;
+    }
+}
